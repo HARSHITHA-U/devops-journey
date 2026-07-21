8ae7a42be0730405c9c05f3996cf5fd890185659
HARSHITHA-U <thashihar@gmail.com>
2026-07-21 14:36:18 +0200

    Add k8s manifests: Deployment, Service, Configmap; app reads config via env var

--- myapp/app.js
+++ myapp/app.js
@@ -1,8 +1,9 @@
 const http = require('http');
+const message = process.env.APP_MESSAGE || 'Default message - no config found';
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello! This is my first DevOps app, running live.\n');
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end(message + '\n');
 });
 
 server.listen(8000, () => {
