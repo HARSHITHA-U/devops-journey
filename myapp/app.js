af486bf656893c9361f42ee8c6e8daba4bc12eb0
HARSHITHA-U <thashihar@gmail.com>
2026-08-05 10:51:30 +0200

    Add Prometheus metrics endpoint to app, update Dockerfile for npm dependencies

--- myapp/app.js
+++ myapp/app.js
@@ -1,8 +1,27 @@
+const client = require('prom-client');
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+const requestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests received',
+  registers: [register]
+});
+
 const http = require('http');
 const message = process.env.APP_MESSAGE || 'Default message - no config found';
 const apiKey = process.env.API_KEY || 'no-key-found';
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+
+  requestCounter.inc();
+
+  if (req.url === '/metrics') {
+    res.writeHead(200, { 'Content-Type': register.contentType });
+    res.end(await register.metrics());
+    return;
+  }
+
   res.writeHead(200, { 'Content-Type': 'text/plain' })
   res.end(message + ' | API_KEY loaded: ' + (apiKey !== 'no-key-found') + '\n');
 });
